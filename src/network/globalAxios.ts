53bfb97299a17525a768c48dbbfcd9ccc889b031
Silas Rhyneer <silas.rhyneer@concordusa.com>
2024-09-17 00:42:13 -0500

    fix: axios will use fetch token method instead

--- src/network/globalAxios.ts
+++ src/network/globalAxios.ts
@@ -5,28 +5,30 @@ const mainAxios = axios.create({
 });
 
 export const createAxios = (
-  token: string,
   url: string,
+  getToken: () => string,
   onUnauthorized: (error: any) => void
 ) => {
-  mainAxios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  mainAxios.defaults.headers.common["Authorization"] = `Bearer ${getToken()}`;
   mainAxios.defaults.baseURL = url;
 
   mainAxios.interceptors.request.use(
     (config) => {
-      try {
-        console.debug(
-          config.method +
-            " " +
-            config.url +
-            " " +
-            (config.data ? JSON.stringify(config.data, null, 2) : "{}")
-        );
-        return config;
-      } catch (e) {
-        console.error("Error in request interceptor:", e);
-        return config;
+      const newConfig = { ...config };
+      const token = getToken();
+      if (token) {
+        const authString = "Bearer " + token;
+        newConfig.headers.Authorization = authString;
       }
+      console.debug(
+        newConfig.method +
+          " " +
+          newConfig.url +
+          " " +
+          (newConfig.data ? JSON.stringify(newConfig.data, null, 2) : "{}")
+      );
+      config = newConfig;
+      return newConfig;
     },
     (error) => {
       console.error("http request error:", JSON.stringify(error, null, 2));
@@ -60,7 +62,7 @@ export const createAxios = (
         prettyError.message = error.response.data; // override message with server response if it exists
       }
       if (error.response?.status === 401) {
-        onUnauthorized(prettyError);
+        onUnauthorized(error);
       }
       return { error: prettyError, failure: true };
     }
