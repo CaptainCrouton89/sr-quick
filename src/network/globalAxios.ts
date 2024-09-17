0ef62c2ab16cc874d1a7fc0f3db8d56c70013aa5
Silas Rhyneer <silas.rhyneer@concordusa.com>
2024-09-16 23:56:24 -0500

    feat: init commit

---
+++ src/network/globalAxios.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+
+const mainAxios = axios.create({
+  timeout: 10000,
+});
+
+export const createAxios = (
+  token: string,
+  url: string,
+  onUnauthorized: (error: any) => void
+) => {
+  mainAxios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  mainAxios.defaults.baseURL = url;
+
+  mainAxios.interceptors.request.use(
+    (config) => {
+      try {
+        console.debug(
+          config.method +
+            " " +
+            config.url +
+            " " +
+            (config.data ? JSON.stringify(config.data, null, 2) : "{}")
+        );
+        return config;
+      } catch (e) {
+        console.error("Error in request interceptor:", e);
+        return config;
+      }
+    },
+    (error) => {
+      console.error("http request error:", JSON.stringify(error, null, 2));
+    }
+  );
+
+  mainAxios.interceptors.response.use(
+    (response) => response.data,
+    async (error) => {
+      const prettyError = {
+        message: error.message || "An error occurred",
+        request: {
+          method: error.config.method,
+          url: error.config.baseURL + error.config.url,
+          data: {},
+        },
+        response: {},
+      };
+      if (error.config.data) {
+        prettyError.request.data = error.config.data
+          .toString()
+          .replace(/"/g, "")
+          .replace(/,/g, ", ")
+          .replace(/:/g, ": ");
+      }
+      if (error.response) {
+        prettyError.response = {
+          status: error.response.status,
+          data: error.response.data,
+        };
+        prettyError.message = error.response.data; // override message with server response if it exists
+      }
+      if (error.response?.status === 401) {
+        onUnauthorized(prettyError);
+      }
+      return { error: prettyError, failure: true };
+    }
+  );
+  return mainAxios;
+};
+
+export interface AxiosError {
+  message: string;
+  request: {
+    method: string;
+    url: string;
+    data: string;
+  };
+  response?: {
+    status: number;
+    data: string;
+  };
+}
+
+export { mainAxios as axios };
---
+++ src/slices/sliceUtils.ts
@@ -0,0 +1,90 @@
+// Purpose: create utility functions used by other slices
+import {
+  CreateSliceOptions,
+  GetThunkAPI,
+  PayloadAction,
+  Slice,
+  SliceCaseReducers,
+  SliceSelectors,
+  UnknownAction,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { AxiosError } from "axios";
+
+export function isPendingAction(sliceName: string) {
+  return (action: UnknownAction): action is PayloadAction =>
+    action.type.startsWith(sliceName) && action.type.endsWith("/pending");
+}
+
+export function isFulfilledAction(sliceName: string) {
+  return (
+    action: UnknownAction
+  ): action is PayloadAction<unknown, string, any, Error> =>
+    action.type.startsWith(sliceName) && action.type.endsWith("/fulfilled");
+}
+
+export function isRejectedAction(sliceName: string) {
+  return (action: PayloadAction) =>
+    action.type.startsWith(sliceName) && action.type.endsWith("/rejected");
+}
+
+export function rejectIfError(response: any, _: GetThunkAPI<any>) {
+  if (response.failure) {
+    throw JSON.stringify(response.error);
+  }
+  return response;
+}
+
+export const withOptimisticState = <T>(initialState: T, objects: string[]) => {
+  return {
+    ...initialState,
+    prevStates: { ...initialState },
+    optimisticLoads: objects.reduce((acc: any, obj: any) => {
+      acc[obj] = { start: 0, stop: 0 };
+      return acc;
+    }, {}),
+  };
+};
+
+interface RejectAction {
+  error: Error | { message: string };
+  axiosError: AxiosError;
+  type: string;
+  meta: any;
+}
+
+export function createSliceWithDefaults<
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string,
+  Selectors extends SliceSelectors<State>,
+  ReducerPath extends string = Name
+>(
+  options: CreateSliceOptions<State, CaseReducers, Name, ReducerPath, Selectors>
+): Slice<State, CaseReducers, Name, ReducerPath, Selectors> {
+  return createSlice({
+    ...options,
+    extraReducers: (builder) => {
+      if (options.extraReducers) options.extraReducers(builder);
+      const sliceName = options.name;
+
+      builder.addMatcher(isFulfilledAction(sliceName), (state: any, _) => {
+        state.status = "idle";
+      });
+      builder.addMatcher(isPendingAction(sliceName), (state: any, _) => {
+        state.status = "loading";
+      });
+      builder.addMatcher(
+        isRejectedAction(sliceName),
+        (state: any, action: RejectAction) => {
+          state.status = "failed";
+          try {
+            state.error = JSON.parse(action.error?.message).message;
+          } catch (e) {
+            state.error = action.error.message;
+          }
+        }
+      );
+    },
+  });
+}
